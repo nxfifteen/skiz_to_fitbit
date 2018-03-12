e114c5829a4140921d77f7a907e818ed36bad6d1
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2018-03-12 12:19:20 +0000

    feat(ux): Added policy pages

--- views/comps/html.navBar.php
+++ views/comps/html.navBar.php
@@ -1,5 +1,14 @@
 <ul class="nav">
     <li class="nav-item">
-        <a class="nav-link" href="/dashboard"><i class="icon-speedometer"></i> Dashboard</a>
+        <a class="nav-link" href="/dashboard"><i class="fa fa-support"></i> Introduction</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/upload"><i class="fa fa-cloud-upload"></i> Upload</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/docs/policy"><i class="fa fa-certificate"></i> Policy</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/docs/tos"><i class="fa fa-book"></i> TOS</a>
     </li>
 </ul>
---
+++ views/content/docs/tos.php
@@ -0,0 +1,79 @@
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                Card title
+            </div>
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+            <div class="card-footer">Card footer</div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <i class="fa fa-check"></i>Card with icon
+            </div>
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                Card with switch
+                <label class="switch switch-sm switch-text switch-info float-right mb-0">
+                    <input class="switch-input" type="checkbox">
+                    <span class="switch-label" data-on="On" data-off="Off"></span>
+                    <span class="switch-handle"></span>
+                </label>
+            </div>
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                Card with label
+                <span class="badge badge-success float-right">Success</span>
+            </div>
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                Card with label
+                <span class="badge badge-pill badge-danger float-right">42</span>
+            </div>
+            <div class="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                ea commodo consequat.
+            </div>
+        </div>
+    </div>
+
+</div>
