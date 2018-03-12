351c3e0c9922867ea65f848add8bceb97d326027
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2018-03-12 17:25:57 +0000

    fix(backend): Delete empty folder as part of cron

--- cron.php
+++ cron.php
@@ -1,7 +1,15 @@
 <?php
+
+    require_once( dirname(__FILE__) . "/lib/autoloader.php" );
+
     $cacheDir = dirname(__FILE__) . '/cache';
     if ( file_exists($cacheDir) AND is_writable($cacheDir) ) {
+
+        nxr(0, "Cleaning up downloads");
+        nxr(1, "Started...");
         cleanUpDownlods($cacheDir, time());
+        RemoveEmptySubFolders($cacheDir);
+        nxr(1, "...Completed");
     }
 
     function cleanUpDownlods($dir, $now)
@@ -11,6 +19,7 @@
             if ($file != "." && $file != "..") {
                 if (is_file($dir . "/" . $file)) {
                     if ($now - filemtime($dir . "/" . $file) >= 60 * 20) { // 2 days
+                        nxr(2, "Deleting " . $dir . "/" . $file);
                         unlink($dir . "/" . $file);
                     }
                 } else if (is_dir($dir . "/" . $file)) {
@@ -18,4 +27,14 @@
                 }
             }
         }
+    }
+
+    function RemoveEmptySubFolders($path)
+    {
+        $empty=true;
+        foreach (glob($path.DIRECTORY_SEPARATOR."*") as $file)
+        {
+            $empty &= is_dir($file) && RemoveEmptySubFolders($file);
+        }
+        return $empty && rmdir($path);
     }
